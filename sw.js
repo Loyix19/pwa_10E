f8d207496408b9f029ba269a418d7eb24bb98b62
Loyix19 <carlosmora1924@gmail.com>
2025-10-22 18:37:20 -0600

    Update sw.js
    SW

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-self.addEventListener ( 'fetch',
-    function(evento) {
-        console.log(evento.request.url);
-
-        if (/\.jpg$/.test(evento.request.url)) {
-            evento.respondWith ( fetch('Bocchi.jpg') );
-        }
-        else if (/\.png$/.test(evento.request.url)) {
-            evento.respondWith(fetch('ado.jpg'));
-        }
+self.addEventListener('fetch',function(event)){
+    if(/\.jpg$/.test(event.requet.url)) {
+        event.respondWith(
+            new Response('<p>Respuesta positiva desde tu service worker UwU!</p>',{
+                header:{'Content-Type':'text/html'}
+            } )
     }
-)
+}
